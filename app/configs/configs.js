cd6123782695d1a719130e61db9593e0b92ac1d6
SummySumanth <sumanth261094@gmail.com>
2025-01-03 03:10:08 +0530

    fix: update start script to specify localhost for serve on port 5001

--- app/configs/configs.js
+++ app/configs/configs.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NODE_ENV === 'production' ? 'https://summy.dev' : 'http://localhost:8080';
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://api.summy.dev' : 'http://localhost:8080';
 const ENV = process.env.NODE_ENV;
 
 export { API_URL, ENV };
