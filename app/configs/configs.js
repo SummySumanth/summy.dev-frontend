f39eb464c5977aa0e1492b416f2758aa915e48a3
Summy Sumanth <sumanth261094@gmail.com>
2025-01-04 19:20:02 +0530

    Linter fixes (#2)
    * fix: update AnimatedRoutes to use API_URL for resume download and improve environment handling
    
    * feat: add environment configuration for development and production; refactor API_URL handling
    
    * fix: simplify TopBar imports and disable prop-types rule in ESLint configuration

--- app/configs/configs.js
+++
@@ -1,7 +0,0 @@
-const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://api.summy.dev'
-    : 'http://localhost:8080';
-const ENV = process.env.NODE_ENV;
-
-export { API_URL, ENV };
--- app/global.d.ts
+++ app/global.d.ts
@@ -7,3 +7,14 @@ declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_ENV: string;
+  // Add other environment variables here
+  // readonly VITE_ANOTHER_ENV_VAR: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
